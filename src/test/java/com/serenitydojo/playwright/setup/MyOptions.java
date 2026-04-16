fe514c24c225a152ec9cfb64522aa3213d5e182c
unknown <jdriddell1@gmail.com>
2026-04-16 13:41:06 +0100

    Updated browser options

--- src/test/java/com/serenitydojo/playwright/setup/MyOptions.java
+++ src/test/java/com/serenitydojo/playwright/setup/MyOptions.java
@@ -1,4 +1,20 @@
 package com.serenitydojo.playwright.setup;
 
-public class MyOptions {
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.junit.Options;
+import com.microsoft.playwright.junit.OptionsFactory;
+
+import java.util.Arrays;
+
+public class MyOptions implements OptionsFactory {
+
+    @Override
+    public Options getOptions() {
+        return new Options()
+                .setHeadless(false)
+                .setLaunchOptions(
+                        new BrowserType.LaunchOptions()
+                                .setArgs(Arrays.asList("--no-sandbox","--disable-gpu"))
+                );
+    }
 }
